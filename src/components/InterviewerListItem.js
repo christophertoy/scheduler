c479531f1f9c2538b6b426ec4af1155871824e51
Christopher Toy <christopherstoy@gmail.com>
2020-09-29 16:33:47 -0600

    added button, daylist, interviewerlist

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "components/InterviewerListItem.scss";
+
+var classNames = require('classnames');
+
+export default function InterviewerListItem(props) {
+  console.log(props)
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected":props.selected
+  });
+
+  const interviewerImageClass = classNames("interviewers__item-image", {
+    "interviewers__item-image--selected-image":props.selected
+  });
+
+return (
+  <li className={ interviewerClass } onClick={props.setInterviewer}>
+  <img
+    className={interviewerImageClass}
+    src={props.avatar}
+    alt={props.name}
+  />
+  {props.selected && props.name}
+</li>
+)
+}
+
