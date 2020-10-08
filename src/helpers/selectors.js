1e8e8e6e22dfe554704c4b483fc653a7c126727c
Christopher Toy <christopherstoy@gmail.com>
2020-10-07 19:58:08 -0600

    deleted comments

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,38 +1,35 @@
-
 export function getAppointmentsForDay(state, day) {
+  const filteredDay = state.days.filter(
+    (selectedDay) => selectedDay.name === day
+  );
 
-const filteredDay = state.days.filter(selectedDay => selectedDay.name === day);
-
-if (filteredDay.length === 0) {
-  return [];
-}
-
-const appointments = filteredDay[0].appointments.map(appointment => {
-  return state.appointments[appointment];
-}) 
-
-return appointments;
-
-}
-
-export function getInterviewersForDay(state, day) {
-
-  // Find the right day
-  const filteredDay = state.days.filter(selectedDay => selectedDay.name === day);
-
-  
   if (filteredDay.length === 0) {
     return [];
   }
-  
-  // Return array with all the interviewer objects of that day
-  const mappedInterviewers = filteredDay[0].interviewers.map(interviewerID => {
-    return state.interviewers[interviewerID];
-   
-  })
- 
-  return mappedInterviewers
 
+  const appointments = filteredDay[0].appointments.map((appointment) => {
+    return state.appointments[appointment];
+  });
+
+  return appointments;
+}
+
+export function getInterviewersForDay(state, day) {
+  const filteredDay = state.days.filter(
+    (selectedDay) => selectedDay.name === day
+  );
+
+  if (filteredDay.length === 0) {
+    return [];
+  }
+
+  const mappedInterviewers = filteredDay[0].interviewers.map(
+    (interviewerID) => {
+      return state.interviewers[interviewerID];
+    }
+  );
+
+  return mappedInterviewers;
 }
 
 export function getInterview(state, interview) {
@@ -40,12 +37,9 @@ export function getInterview(state, interview) {
     return null;
   }
 
-  const interviewObj = { student: interview.student }
+  const interviewObj = { student: interview.student };
 
-  interviewObj.interviewer = state.interviewers[interview.interviewer]
+  interviewObj.interviewer = state.interviewers[interview.interviewer];
 
-  return interviewObj
-
-
+  return interviewObj;
 }
-
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -7,17 +7,13 @@ const useVisualMode = (initial) => {
   function transition(newMode, replace = false) {
     if (replace) {
       history.push(initial);
-  
     }
-  
+
     setHistory([...history, newMode]);
     setMode(newMode);
-
   }
 
   function back() {
-
-
     if (history.length > 1) {
       history.pop();
 
