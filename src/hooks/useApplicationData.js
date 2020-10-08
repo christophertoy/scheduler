909ff2e5daa7592a72ee25d1c29042830ecd85f0
Christopher Toy <christopherstoy@gmail.com>
2020-10-07 19:31:06 -0600

    refactored and cleaned up code

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,108 +1,94 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const useApplicationData = () => {
-  
   const [state, setState] = useState({
-    day:"Monday",
+    day: "Monday",
     days: [],
     appointments: {},
     interviewers: {},
   });
-  
-  const setDay = day => setState({ ...state, day});
 
-  
-  function cancelInterview(id){
+  const setDay = (day) => setState({ ...state, day });
+
+  function cancelInterview(id) {
     const appointment = {
       ...state.appointments[id],
-      interview: null
+      interview: null,
     };
-    
+
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
 
- 
     const days = [...state.days];
-    // loop through the index of the days
+
     for (const index in days) {
-      // set day to current index of days
       const day = days[index];
 
-      // loop through appointments
       for (const apptID of day.appointments)
-      // check to see if id matches
-       if (apptID === id) {
-        // update day, and update spots + 1
-         days[index] = {...day, spots: day.spots + 1}
-       }
-
+        if (apptID === id) {
+          days[index] = { ...day, spots: day.spots + 1 };
+        }
     }
-    
-    return axios.delete(`/api/appointments/${id}`, appointment)
-    .then(() => {
-      setState({...state, appointments, days})
-      console.log("deleted successfully")
-    })
-    
+
+    return axios.delete(`/api/appointments/${id}`, appointment).then(() => {
+      setState({ ...state, appointments, days });
+      console.log("deleted successfully");
+    });
   }
-  
+
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview }
+      interview: { ...interview },
     };
-    
+
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
-    
-    const days = [...state.days];
-    // loop through the index of the days
-    for (const index in days) {
-      // set day to current index of days
-      const day = days[index];
-      // loop through appointments
-      
-      for (const apptID of day.appointments)
-      // check to see if id matches
-       if (apptID === id && state.appointments[id].interview === null) {
-        // update day, and update spots - 1
-         days[index] = {...day, spots: day.spots - 1}
-       } 
 
+    const days = [...state.days];
+    for (const index in days) {
+      const day = days[index];
+
+      for (const apptID of day.appointments)
+        if (apptID === id && state.appointments[id].interview === null) {
+          days[index] = { ...day, spots: day.spots - 1 };
+        }
     }
 
-    return axios.put(`/api/appointments/${id}`, appointment)
-    .then((response) => {
-      console.log("added sucessfully")
-      setState({...state, appointments, days})
-    })
+    return axios
+      .put(`/api/appointments/${id}`, appointment)
+      .then((response) => {
+        console.log("added sucessfully");
+        setState({ ...state, appointments, days });
+      });
   }
 
   useEffect(() => {
-
     Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ])
-      .then((all) => {
-        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-      })
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
+  }, []);
 
-  }, [])
-  
-  
   return {
     state,
     setDay,
     bookInterview,
-    cancelInterview
-  }
+    cancelInterview,
+  };
 };
 
 export default useApplicationData;
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,41 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const useVisualMode = (initial) => {
-
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(newMode, replace = false) {
-
     if (replace) {
       history.push(initial);
-      // history.pop()?
+  
     }
-    // history.push(newMode);
+  
     setHistory([...history, newMode]);
     setMode(newMode);
-      // copy history array
-      // const copyHistory = [...history, newMode]
-
-    }
-  
-  function back() {
-    // const copyHistory = [...history]
-    // console.log('before', copyHistory)
-    // copyHistory.pop()
-    // console.log('after', copyHistory)
-
-    if (history.length > 1) {
-      history.pop()
-   
-      setMode(history[history.length-1])
-
-    }
-  
 
   }
 
-  return { mode, transition, back }
+  function back() {
+
+
+    if (history.length > 1) {
+      history.pop();
+
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
 };
 
 export default useVisualMode;
